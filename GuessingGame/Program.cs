e7714542c81d2cede874fc31ef622fd8217bb7f6
markusanton <markusa070@gmail.com>
2020-09-24 12:29:33 +0300

    Add project files.

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 11);
+
+            Console.WriteLine("Enter a number from 1 to 10.");
+            int userNumber = Int32.Parse(Console.ReadLine());
+            bool guessState = false;
+
+            while (!guessState)
+            {
+                if (randomNumber == userNumber)
+                {
+                    Console.WriteLine("You won!");
+                    guessState = true;
+                }
+                else
+                {
+                    Console.WriteLine("Try again! Enter a number from 1 to 10.");
+                    userNumber = Int32.Parse(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
---
+++ WhileLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int randomNumber = rnd.Next(1, 11);
+
+            Console.WriteLine("Enter a number from 1 to 10.");
+            int userNumber = Int32.Parse(Console.ReadLine());
+
+            if (userNumber == randomNumber)
+            {
+                Console.WriteLine($"You guessed the number. It was {randomNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't guess the number. It was {randomNumber}, not {userNumber}.");
+            }
+        }
+    }
+}
